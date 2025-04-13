9b6e26296ed49964b3667c8e00906bfbde75ff42
Panipurii <86043589+Ashish-Maharana188@users.noreply.github.com>
2025-04-14 00:54:56 +0530

    ...

--- src/components/notesModule/NotesTimelineFilter.tsx
+++ src/components/notesModule/NotesTimelineFilter.tsx
@@ -1,11 +1,5 @@
 import React from "react";
-import {
-  FormControl,
-  InputLabel,
-  Select,
-  MenuItem,
-  Switch,
-} from "@mui/material";
+import { FormControl, Select, MenuItem, Switch } from "@mui/material";
 import { NotesTimelineFilterProps } from "../../TS_INTERFACE/gInterface";
 
 const NotesTimelineFilter: React.FC<NotesTimelineFilterProps> = ({
@@ -19,43 +13,58 @@ const NotesTimelineFilter: React.FC<NotesTimelineFilterProps> = ({
   setIs24Hour,
 }) => {
   return (
-    <div className="flex justify-between items-center p-4 bg-white shadow-md rounded-lg mb-4">
+    <div className="flex justify-between items-center p-4 bg-white shadow-md rounded-lg ">
       {/* Interval Filter */}
-      <FormControl size="small">
-        <InputLabel>Interval</InputLabel>
-        <Select
-          value={interval}
-          onChange={(e) => setInterval(e.target.value as "6h" | "12h" | "24h")}
-        >
-          <MenuItem value="6h">6 Hours</MenuItem>
-          <MenuItem value="12h">12 Hours</MenuItem>
-          <MenuItem value="24h">24 Hours</MenuItem>
-        </Select>
-      </FormControl>
+      <div className="mr-5">
+        <FormControl size="small">
+          <Select
+            value={interval}
+            MenuProps={{
+              disableScrollLock: true, // 👈 disables the scroll lock behavior
+            }}
+            onChange={(e) =>
+              setInterval(e.target.value as "6h" | "12h" | "24h")
+            }
+          >
+            <MenuItem value="6h">6 Hours</MenuItem>
+            <MenuItem value="12h">12 Hours</MenuItem>
+            <MenuItem value="24h">24 Hours</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
 
       {/* AM/PM Filter */}
-      <FormControl size="small">
-        <InputLabel>AM/PM</InputLabel>
-        <Select
-          value={selectedAMPM}
-          onChange={(e) => setSelectedAMPM(e.target.value as "AM" | "PM")}
-        >
-          <MenuItem value="AM">AM</MenuItem>
-          <MenuItem value="PM">PM</MenuItem>
-        </Select>
-      </FormControl>
+      <div className="mr-5">
+        <FormControl size="small">
+          <Select
+            value={selectedAMPM}
+            MenuProps={{
+              disableScrollLock: true, // 👈 disables the scroll lock behavior
+            }}
+            onChange={(e) => setSelectedAMPM(e.target.value as "AM" | "PM")}
+          >
+            <MenuItem value="AM">AM</MenuItem>
+            <MenuItem value="PM">PM</MenuItem>
+            <MenuItem value="null">None</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
 
       {/* Sort Order */}
-      <FormControl size="small">
-        <InputLabel>Sort Order</InputLabel>
-        <Select
-          value={sortOrder}
-          onChange={(e) => setSortOrder(e.target.value as "asc" | "desc")}
-        >
-          <MenuItem value="asc">Ascending</MenuItem>
-          <MenuItem value="desc">Descending</MenuItem>
-        </Select>
-      </FormControl>
+      <div className="mr-5">
+        <FormControl size="small">
+          <Select
+            value={sortOrder}
+            MenuProps={{
+              disableScrollLock: true, // 👈 disables the scroll lock behavior
+            }}
+            onChange={(e) => setSortOrder(e.target.value as "asc" | "desc")}
+          >
+            <MenuItem value="asc">Ascending</MenuItem>
+            <MenuItem value="desc">Descending</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
 
       {/* 12h/24h Switch */}
       <div className="flex items-center">
