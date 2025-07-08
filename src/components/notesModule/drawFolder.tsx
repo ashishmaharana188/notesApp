7d5009bbc83f391d7286c386d127795b1339b4ea
Panipurii <86043589+Ashish-Maharana188@users.noreply.github.com>
2025-07-09 01:27:03 +0530

    asd

--- src/components/notesModule/drawFolder.tsx
+++ src/components/notesModule/drawFolder.tsx
@@ -16,35 +16,61 @@ class FilesDraw extends Component<NoteListProps> {
             {notes.map((note: notesReducerIntf, index: number) => (
               <div
                 key={index}
-                className="relative bg-black rounded-xl  border w-full h-full pl-220 pr-220 pt-400 z-0"
+                className="relative bg-whitesmoke rounded-xl w-full h-full pl-220 pr-220 pt-400 z-0"
               >
                 <div>
                   <div
-                    className="text-3xl text-center w-333 bg-white border rounded-xl h-20 absolute top-290 left-220 -translate-x-1/2 -translate-y-1/2 z-1"
-                    style={{
-                      clipPath: "polygon(0% 0%, 100% 0%, 99% 100%, 1% 100%)",
-                    }}
+                    className="text-3xl text-center w-333 bg-white border-2 rounded-xl w-333 h-20 absolute top-310 left-220 -translate-x-1/2 -translate-y-1/2 z-1"
+                    style={{ transform: "skewX(0deg)", overflow: "hidden" }}
                   >
                     {note.title}
                   </div>
+
                   <div>
-                    <div className="text-2xl bg-black text-white text-center w-50 h-20 rounded-xl border absolute top-292 left-105 -translate-x-1/2 -translate-y-1/2 z-1">
-                      edge
+                    <div
+                      className="top-310 left-100 absolute -translate-x-1/2 -translate-y-1/2 z-1"
+                      style={{ transform: "rotatey(20deg)" }}
+                    >
+                      <svg
+                        viewBox="0 0 400 200"
+                        className="w-[200px] h-[100px]"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="
+            M 60 130
+            L 100 50
+            Q 110 40, 125 40
+            H 280
+            Q 295 40, 305 60
+            L 330 105
+            Z
+          "
+                          fill="black"
+                          stroke="white"
+                          strokeWidth="3"
+                        />
+                        <text
+                          x="200"
+                          y="70"
+                          textAnchor="middle"
+                          dominantBaseline="middle"
+                          className="fill-white text-6xl"
+                        >
+                          {note.title.charAt(0)}
+                        </text>
+                      </svg>
                     </div>
                     <div
-                      className="text-4xl text-center  bg-white rounded-xl w-333 border-t-2 h-20 absolute top-300 left-220 -translate-x-1/2 -translate-y-1/2  z-1"
-                      style={{
-                        clipPath: "polygon(0% 0%, 100% 0%, 99% 100%, 1% 100%)",
-                      }}
-                    >
-                      {note.title.charAt(0)}
-                    </div>
+                      className="text-3xl text-center w-333 bg-white border-2 rounded-xl w-333 h-20 absolute top-320 left-220 -translate-x-1/2 -translate-y-1/2 z-1"
+                      style={{ transform: "skewX(0deg)", overflow: "hidden" }}
+                    ></div>
                   </div>
-                  <div className="absolute rounded-xl  bg-white left-50 bottom-74 w-1 h-300"></div>
-                  <div className="absolute rounded-xl  bg-white right-50 bottom-74 w-1 h-300"></div>
-                  <div className="absolute rounded-xl border bg-white right-21.5 bottom-34 w-2 h-40 rotate-5 "></div>
-                  <div className="absolute rounded-sm border w-400 bottom-70 left-20 bg-white h-4 z-1"></div>
-                  <div className="absolute rounded-xl border bg-white left-21.5 bottom-34 w-2 h-40 -rotate-5"></div>
+                  <div className="absolute rounded-xl border-2 bg-white left-50 bottom-74 w-1 h-300"></div>
+                  <div className="absolute rounded-xl  border-2 bg-white right-50 bottom-74 w-1 h-300"></div>
+                  <div className="absolute rounded-xl border-3 bg-white right-21.5 bottom-34 w-2 h-40 rotate-5 "></div>
+                  <div className="absolute rounded-sm border-3 w-400 bottom-70 left-20 bg-white h-4 z-2"></div>
+                  <div className="absolute rounded-xl border-3 bg-white left-21.5 bottom-34 w-2 h-40 -rotate-5"></div>
                 </div>
               </div>
             ))}
